2ac1ed5a213a2c3ec2818193b8120b5cd71f8eca
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-02 22:30:54 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace AllDartling;
+
+public static class ModHelperData
+{
+    public const string Name = "All Dartling";
+    public const string Description = "Makes all towers and paragons shoot with mouse control like dartling gunner.";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "Myself-Bloons";
+    public const string RepoName = "All_Dartling";
+    public const string Author = "Myself";
+    public const string WorksOnVersion = "51.2";
+    public const string DllName = "AllDartling.dll";
+    public const string Icon = "Icon.png";
+}
